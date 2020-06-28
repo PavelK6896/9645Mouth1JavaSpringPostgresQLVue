c4aa941bc739d3246dff1b26704b143842c8f0e6
PavelK6896 <79208776896@yandex.ru>
2020-06-28 22:20:24 +0300

    "add form adding, deleting message"

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,44 +1,114 @@
-var messageApi = Vue.resource('/message{/id}'); //вьюресурс {}-часть пути не обезательное
 
-Vue.component('message-row', {//отоброжает строку
-    props: ['message'],//принемает часть масива
-    //темплате для листа
-    template: '<div>' +
-        '<i>({{ message.id }})</i> {{ message.text }}' +
-        '</div>'
+function getIndex(list, id) { //определяет индекс в колекции
+    for (var i = 0; i < list.length; i++ ) {
+        if (list[i].id === id) {
+            return i;
+        }
+    }
+
+    return -1;
+}
+
+
+var messageApi = Vue.resource('/message{/id}'); //для rest
+
+Vue.component('message-form', { //форма для добавления  и изменения
+    props: ['messages', 'messageAttr'], //
+    data: function() { // вункция возвращает объект что бы переменые была уникальная внутри компанента
+        return {//стате в компаненте ву локальный
+            text: '',
+            id: ''
+        }
+    },
+    watch: { //следит за messageAttr или обновляет стате
+        messageAttr: function(newVal, oldVal) {
+            this.text = newVal.text; //устанавливает новое значание
+            this.id = newVal.id;
+        }
+    },
+    template:
+        '<div>' + //фома в ву для ресурсе
+            '<input type="text" placeholder="Write something" v-model="text" />' +//v-model связывает объект с локальным стетом
+            '<input type="button" value="Save" @click="save" />' + //v-on:click или @click вызывает метод
+        '</div>',
+    methods: {
+        save: function() { //метод для кнопки как сабмит
+            const message = { text: this.text };//сообщение
+
+            if (this.id) { //если обнавление
+                messageApi.update({id: this.id}, message).then(result =>
+                    result.json().then(data => {
+                        const index = getIndex(this.messages, data.id); //находим индекс
+                        this.messages.splice(index, 1, data); // меняем элемент по индексу
+                        this.text = ''// очищаем форму ввода
+                        this.id = ''// очищаем форму ввода
+                    })
+                )
+            } else {
+                messageApi.save({}, message).then(result => // метод save post запрос
+                    result.json().then(data => { // верныл созданые объект с id
+                        this.messages.push(data); //добавляем в наш список
+                        this.text = '' // очищаем форму ввода
+                    })
+                )
+            }
+        }
+    }
 });
 
-Vue.component('messages-list', {//отоброжает лист
-  props: ['messages'],//компонент принемает масив из корня
+Vue.component('message-row', {//для строчьки
+    props: ['message', 'editMethod', 'messages'],
+    template: '<div>' +
+        '<i>({{ message.id }})</i> {{ message.text }}' +
+        '<span style="position: absolute; right: 0">' +
+            '<input type="button" value="Edit" @click="edit" />' + //редактирование
+            '<input type="button" value="X" @click="del" />' +//удаление
+        '</span>' +
+        '</div>',
+    methods: {
+        edit: function() {
+            this.editMethod(this.message); // используем полученые месадж
+        },
+        del: function() { //this потомучто поле
+            messageApi.remove({id: this.message.id}).then(result => { //проверяем существования
+                if (result.ok) { //если есть тоесть удален на сервере
+                    this.messages.splice(this.messages.indexOf(this.message), 1) //удаляем на фронте
+                }
+            })
+        }
+    }
+});
 
-//переберает заполненый масив
+Vue.component('messages-list', {//для списка
+  props: ['messages'],
+  data: function() {
+    return {
+        message: null
+    }
+  },
   template:
     '<div style="position: relative; width: 300px;">' +
-      //ключь по ид
-        '<message-row v-for="message in messages" :key="message.id" :message="message" ' + // прокидываем части масива
-            '/>' +
+        '<message-form :messages="messages" :messageAttr="message" />' + //форма добавления
+        '<message-row v-for="message in messages" :key="message.id" :message="message" ' + //цикол фор
+            ':editMethod="editMethod" :messages="messages" />' +
     '</div>',
-
-
-    //заполняем мосив
-    //--------------------------------------resource--
-    //получаем данные с сервера и ложем в messages
-    // на момент создания объекта не существует
-    //если () => то this for window
-  created: function() {//ХУК ву когда проинициализировали объект
-    messageApi.get().then(result => // запрок к созданому ву ресурс //промис возращает //result ответ сервера
-    //статус текст json - response
-        result.json().then(data => //так как промис то then // получили json с сервера
-            data.forEach(message => this.messages.push(message)) //ложем json  messages по сылке который
+  created: function() {
+    messageApi.get().then(result =>
+        result.json().then(data =>
+            data.forEach(message => this.messages.push(message))
         )
     )
   },
-    //*********************************************
+  methods: {
+    editMethod: function(message) { //пробрасывет в форму редактирования
+        this.message = message;
+    }
+  }
 });
 
 var app = new Vue({
   el: '#app',
-  template: '<messages-list :messages="messages" />',//передает значение в компонент//прокидывает пустой мосив
+  template: '<messages-list :messages="messages" />',
   data: {
     messages: []
   }
